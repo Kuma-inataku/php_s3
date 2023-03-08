217c8264aa6fc64ae1910fdfc1ba47bddffd39f0
takuya-inaguma <inataku0121@gmail.com>
2023-03-08 22:43:01 +0900

    create s3 instance

--- upload.php
+++ upload.php
@@ -4,4 +4,24 @@ require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 use Aws\CommandPool;
 
-var_dump('hoge');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$credentials = [
+    'key' => $_ENV['AWS_ACCESS_KEY'],
+    'secret' => $_ENV['AWS_SECRET_KEY'],
+];
+
+$bucket_version = $_ENV['BUCKET_VERSION'];
+$bucket_region = $_ENV['BUCKET_REGION'];
+$bucket_name = $_ENV['BUCKET_NAME'];
+
+$s3 = new S3Client([
+    'credentials' => $credentials,
+    'region'  => $bucket_region,
+    'version' => $bucket_version,
+]);
+
+var_dump('s3 instance created!!!');
+echo '<br>';
+var_dump('s3', $s3);
