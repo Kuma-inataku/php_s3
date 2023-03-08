2f411bcd4409a1e9d680577987747ff625b43c45
takuya-inaguma <inataku0121@gmail.com>
2023-03-08 23:01:44 +0900

    create index

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php echo 1111 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>S3</title>
+</head>
+<body>
+    <div>
+        <h1>S3 UPLOAD</h1>
+        <div>
+            <a href="upload.php">upload</a>
+        </div>
+    </div>
+    <div>
+        <h1>S3 DOWNLOAD</h1>
+        <div>
+            <a href="download.php">download</a>
+        </div>
+    </div>
+</body>
+</html>
