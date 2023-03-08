f2a0e83cc0aa6e65d88e3ff11b0144a303f5c831
takuya-inaguma <inataku0121@gmail.com>
2023-03-08 23:02:26 +0900

    upload s3 logic

--- upload.php
+++ upload.php
@@ -3,6 +3,7 @@ require 'vendor/autoload.php';
 
 use Aws\S3\S3Client;
 use Aws\CommandPool;
+use Aws\S3\Exception\S3Exception;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
@@ -16,12 +17,24 @@ $bucket_version = $_ENV['BUCKET_VERSION'];
 $bucket_region = $_ENV['BUCKET_REGION'];
 $bucket_name = $_ENV['BUCKET_NAME'];
 
+// インスタンス生成
 $s3 = new S3Client([
     'credentials' => $credentials,
     'region'  => $bucket_region,
     'version' => $bucket_version,
 ]);
 
-var_dump('s3 instance created!!!');
-echo '<br>';
-var_dump('s3', $s3);
+$params = [
+    'Bucket' => $bucket_name,
+    'Key' => 's3/sample'.mt_rand(1, 10000).'.jpg',
+    'SourceFile'   => 'image/luffy.png',
+];
+
+try {
+    $result = $s3->putObject($params);
+    var_dump($result['ObjectURL']);
+} catch (S3Exception $e) {
+    echo $e -> getMessage();
+}
+
+var_dump('s3 saved!!!');
