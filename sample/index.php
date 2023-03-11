6741a9ea2d80514f5a0e73b69d84c608f90f3680
takuya-inaguma <inataku0121@gmail.com>
2023-03-11 15:17:15 +0900

    transfer sample pages

---
+++ sample/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>S3</title>
+</head>
+<body>
+    <div>
+        <h1>S3 UPLOAD</h1>
+        <div>
+            <a href="upload.php">upload</a>
+        </div>
+    </div>
+    <div>
+        <h1>S3 DOWNLOAD</h1>
+        <div>
+            <a href="download.php">download</a>
+        </div>
+    </div>
+    <div>
+        <h1>S3 S3 URL</h1>
+        <div>
+            <a href="s3Url.php">s3Url</a>
+        </div>
+    </div>
+</body>
+</html>
