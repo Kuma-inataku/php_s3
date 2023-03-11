18d8fadd561845d81f038b55a6ce8a5dc03c5d48
takuya-inaguma <inataku0121@gmail.com>
2023-03-11 23:18:07 +0900

    brush up list page

--- list.php
+++ list.php
@@ -37,16 +37,24 @@ $bucket = "aws-php-bucket";
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>一覧</title>
 </head>
 <body>
-    <div>
+    <div class="m-3">
+    <h2>一覧</h2>
     <?php foreach ($list['Contents'] as $img) { ?>
         <img src="<?= $baseUrl ?>/<?= $bucket_name ?>/<?= $img['Key'] ?>" height='100' loading='lazy'>
     <?php } ?>
     </div>
+    <div class="m-3">
+        <a href="index.php">HOME</a>
+    </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
